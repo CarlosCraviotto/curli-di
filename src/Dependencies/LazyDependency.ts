a0951616e1f3eed8e58ea2554e247a184c500c9b
LAPTOP-HE20GUS5\carlo <curlirojo@gmail.com>
2020-04-06 02:56:12 +0200

    Reformat

--- src/Dependencies/LazyDependency.ts
+++ src/Dependencies/LazyDependency.ts
@@ -1,18 +1,17 @@
-
 export class LazyDependency {
 
     private appliedCallback: boolean = false;
 
-    public constructor (private dependenciesToCheck: Array<string>, private callback: any){
+    public constructor(private dependenciesToCheck: Array<string>, private callback: any) {
     }
 
-    public dependencyHasBenBuilt(dependency: string){
+    public dependencyHasBenBuilt(dependency: string): void {
         const index = this.dependenciesToCheck.indexOf(dependency);
 
-        if (index !== -1){
-            //delete this.dependenciesToCheck[index];
-            this.dependenciesToCheck.splice (index, 1);
-            if (this.dependenciesToCheck.length <= 0){
+        if (index !== -1) {
+            //delete the dependency
+            this.dependenciesToCheck.splice(index, 1);
+            if (this.dependenciesToCheck.length <= 0) {
                 this.applyCallback();
             }
         }
